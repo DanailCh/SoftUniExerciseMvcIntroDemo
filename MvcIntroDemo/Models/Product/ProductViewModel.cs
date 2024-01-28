13c58335f069fa5b47cd9222380013fb4f5b4679
DanailCh <114429701+DanailCh@users.noreply.github.com>
2024-01-28 12:37:48 +0200

    Add project files.

---
+++ MvcIntroDemo/Models/Product/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcIntroDemo.Models.Product
+{
+	public class ProductViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public double Price { get; set; }
+
+	}
+}
